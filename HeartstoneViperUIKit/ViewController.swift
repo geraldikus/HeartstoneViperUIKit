dd3ff578d5018c96089d59fc65d679755f25cc8b
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2024-02-22 10:48:46 +0400

    Initial commit

---
+++ HeartstoneViperUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeartstoneViperUIKit
+//
+//  Created by Anton on 22.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
